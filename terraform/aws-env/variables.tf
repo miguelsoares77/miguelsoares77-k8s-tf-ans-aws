c0d53283cdf172229ce6192ca7ec0289325ccf57
luis20soares@gmail.com <luis20soares@gmail.com>
2026-06-19 18:22:46 +0100

    test aws vars

--- terraform/aws-env/variables.tf
+++ terraform/aws-env/variables.tf
@@ -1,21 +1,21 @@
-variable "region" {
-  type        = string
-  description = "AWS Infra Region"
-  # default     = "eu-central-1"
-}
+# variable "region" {
+#   type        = string
+#   description = "AWS Infra Region"
+#   # default     = "eu-central-1"
+# }
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_access_key" {
+#   description = "AWS Access Key"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
+# variable "aws_secret_key" {
+#   description = "AWS Secret Key"
+#   type        = string
+#   sensitive   = true
 
-}
+# }
 
 # SECURITY MODULE
 variable "public_key_name" {
