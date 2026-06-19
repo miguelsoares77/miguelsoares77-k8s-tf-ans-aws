7ad19fa1c4f704da206dc920d39883b44ed503f0
luis20soares@gmail.com <luis20soares@gmail.com>
2026-06-19 19:00:08 +0100

    debug aws cred

--- terraform/aws-env/provider.tf
+++ terraform/aws-env/provider.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.40"
+      version = "~> 6.0"
     }
   }
 }
 
 provider "aws" {
   region        = var.region
-  # access_key    = var.aws_access_key
-  # secret_key    = var.aws_secret_key
 }
